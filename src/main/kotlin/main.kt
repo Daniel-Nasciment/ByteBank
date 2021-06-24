718630d47960fe39b5dc3e4551b7d94bd784d61d
Daniel <daniel.fecaf@outlook.com>
2021-06-24 15:10:37 -0300

    Criando construtor

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,18 +1,12 @@
 fun main() {
     println("Bem vindo ao ByteBank")
 
-    //ISSO É UMA VARIÁVEL QUE REFERENCIA UM OBETO DO TIPO CONTA
-    //ISSO É UMA VARIÁVEL QUE REFERENCIA UM OBETO DO TIPO CONTA
-    val contaDaniel = Conta()
-    contaDaniel.titular = "Daniel"
-    contaDaniel.conta = 12345
+    val contaDaniel = Conta("Daniel", 12345)
     contaDaniel.deposito(200.0)
 
     contaDaniel.deposito(800.0)
 
-    val contaMarco = Conta()
-    contaMarco.titular = "Marco"
-    contaMarco.conta = 54321
+    val contaMarco = Conta("Marco",54321)
     contaMarco.deposito(300.0)
 
     contaDaniel.transferencia(500.0, contaMarco)
@@ -22,18 +16,10 @@ fun main() {
 
 }
 
-
-class Conta {
-    var titular = ""
-    var conta = 0
-
-    // POR PADRÃO NO KOTLIN JÁ É ENCAPSULADO OS GETTERS E SETTERS
+class Conta(var titular: String, var conta: Int) {
     var saldo = 0.0
         private set(valor) {
-            if (valor > 0) {
-                // FIELD SERIA O VALOR DO SALDO
-                field = valor
-            }
+            if (valor > 0) field = valor
         }
 
     fun deposito(valor: Double) {
@@ -49,7 +35,6 @@ class Conta {
         }
     }
 
-    // PARA DEFINIR O RETORNO DE UMA FUNÇÃO:
     fun transferencia(valor: Double, destino: Conta): Boolean {
         if (this.saldo >= valor) {
             this.saldo -= valor
@@ -58,34 +43,4 @@ class Conta {
         }
         return false
     }
-}
-
-fun testaCondicoes(saldo: Double) {
-
-    if (saldo > 0.0) {
-        println("Conta positiva!")
-    }
-
-    when {
-        saldo == 0.0 -> println("Conta neutra!")
-    }
-
-    var i = 0
-    while (i < 10) {
-        i++
-        println(i)
-    }
-
-
-//    for (i in 0..3) {
-//        println(i)
-//    }
-//
-//    for (i in 4 downTo 0 step 2) {
-//        // DOWNTO - DE TRAZ PARA FRENTE
-//        // STEP - DE 2 EM 2
-//        println(i)
-//        println()
-//    }
-
 }
