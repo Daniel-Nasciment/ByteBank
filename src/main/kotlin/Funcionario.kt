8a44a044d535d90b56cff83c54f62876740ced32
Daniel <daniel.fecaf@outlook.com>
2021-06-25 00:17:53 -0300

    Trabalhando com herança

--- src/main/kotlin/Funcionario.kt
+++ src/main/kotlin/Funcionario.kt
@@ -1,9 +1,11 @@
-class Funcionario(
+// PARA EXPECIFICAR QUE A CLASSE SERÁ HERDADA É NECESSÁRIO DEFINIR O OPEN
+open class Funcionario(
     val nome: String,
     val cpf: String,
     var salario: Double
+
 ) {
     fun bonificacao(): Double {
-        return salario * 0.5
+        return salario * 0.1
     }
 }
