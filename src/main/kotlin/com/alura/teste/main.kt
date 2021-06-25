50d7aa07983d25e42d9ba0e39b84133141274e0f
Daniel <daniel.fecaf@outlook.com>
2021-06-25 16:38:52 -0300

    Safe call e let para lidar com valores null

--- src/main/kotlin/com/alura/teste/main.kt
+++ src/main/kotlin/com/alura/teste/main.kt
@@ -1,6 +1,16 @@
+import com.alura.modelo.Endereco
+
 fun main() {
 
-    testaComportamentosConta()
+    var enderecoNulo: Endereco? = Endereco("Rua teste")
+
+    // ? SIGNIFICA QUE PODE SER NULL OU NÃO
+    //SAFE CALL -> QUALQUER MEMBRO PRECISA DO '?'
+    enderecoNulo?.logradouro?.length
+
+    enderecoNulo?.let { endereco: Endereco ->
+        println(endereco.logradouro.length)
+    }
 
 }
 
