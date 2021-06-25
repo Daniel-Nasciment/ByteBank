3d3a24f0efd7da3b14f9dc4b966ce6e101b88e08
Daniel <daniel.fecaf@outlook.com>
2021-06-25 05:00:21 -0300

    Variáveis globais

--- src/main/kotlin/com/alura/modelo/Conta.kt
+++ src/main/kotlin/com/alura/modelo/Conta.kt
@@ -1,11 +1,24 @@
 package com.alura.modelo
 
+// ESSA ABORADAGEM N É RECOMENDADA
+// NESSE EXEMPLO SE EU TIVESSE OUTRAS CLASSES DENTRO DESSE ARQUIVO EU CONSEGUIRIA EFETUAR
+// A ALTERAÇÃO NO CONTADOR
+
+var totalContas = 0
+    private set
+
 abstract class Conta(
     val titular: Cliente,
     val conta: Int
 ) {
     var saldo = 0.0
         protected set
+
+
+    init {
+        println("Nova conta criada")
+        totalContas++
+    }
 
     fun deposito(valor: Double) {
         this.saldo += valor
--- src/main/kotlin/com/alura/teste/main.kt
+++ src/main/kotlin/com/alura/teste/main.kt
@@ -1,6 +1,20 @@
+import com.alura.modelo.Cliente
+import com.alura.modelo.ContaPoupanca
+import com.alura.modelo.totalContas
+
 fun main() {
 
-    testaComportamentosConta()
+
+    val daniel = Cliente(
+        nome = "Daniel",
+        cpf = "111.111.111-11",
+        senha = 12345
+    )
+
+    val contaPoupanca = ContaPoupanca(titular = daniel, conta = 1)
+    val contaPoupanca1 = ContaPoupanca(titular = daniel, conta = 1)
+
+    println("Total criadas: ${totalContas}")
 
 }
 
