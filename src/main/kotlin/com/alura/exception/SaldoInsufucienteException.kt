9e02cf3abc4635294a4649eefd22e30087d1c9a2
Daniel <daniel.fecaf@outlook.com>
2021-06-25 15:45:23 -0300

    Criando minha própria exception

---
+++ src/main/kotlin/com/alura/exception/SaldoInsufucienteException.kt
@@ -0,0 +1,4 @@
+package com.alura.exception
+
+// CTRL + H -> MOSTRA A HIERARQUIA DE CLASSES
+class SaldoInsufucienteException(): Exception("Saldo não suficiente.")
--- src/main/kotlin/com/alura/teste/main.kt
+++ src/main/kotlin/com/alura/teste/main.kt
@@ -1,10 +1,7 @@
-import java.lang.ClassCastException
-
 fun main() {
 
-    // ESSA É UMA FORMA DE LANÇAR UMA EXCEPTION
-    throw ClassCastException()
 
 }
+
 
 
