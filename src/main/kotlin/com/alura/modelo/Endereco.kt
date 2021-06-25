6bbfb61f659f05e14cf8bb329e034c0d7685fafc
Daniel <daniel.fecaf@outlook.com>
2021-06-25 06:18:52 -0300

    ToString

--- src/main/kotlin/com/alura/modelo/Endereco.kt
+++ src/main/kotlin/com/alura/modelo/Endereco.kt
@@ -4,5 +4,9 @@ class Endereco(
     var logradouro: String = "",
     var numero: Int = 0
 ) {
-
+    override fun toString(): String {
+        return """
+            |Endereco(logradouro='$logradouro', 
+            |numero=$numero)""".trimMargin()
+    }
 }
--- src/main/kotlin/com/alura/teste/main.kt
+++ src/main/kotlin/com/alura/teste/main.kt
@@ -1,9 +1,12 @@
+import com.alura.modelo.Endereco
+
 fun main() {
 
-    /*todas as classes do Kotlin herdam de Any
-     Ao usarmos a classe Any ficamos restritos aos métodos dela, como é o caso do toString(),
-     equals() e hashCode().
-     Com o uso do Any é necessário o casting para converter o objeto em um tipo esperado.*/
+    val endereco = Endereco()
+
+    println(endereco.toString())
+
+
 }
 
 
