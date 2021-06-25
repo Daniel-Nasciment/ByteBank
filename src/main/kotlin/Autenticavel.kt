26e702fbccaf6daa9f502c619a600d72b1b0bf6c
Daniel <daniel.fecaf@outlook.com>
2021-06-25 03:13:10 -0300

    Anotações sobre interface e outra forma de implementa-las

--- src/main/kotlin/Autenticavel.kt
+++ src/main/kotlin/Autenticavel.kt
@@ -1,5 +1,9 @@
 interface Autenticavel {
 
+    // É POSSIVEL REALIZAR A IMPLEMENTAÇÃO DO MÉTODO DENTRO DA INTERFACE
+    // PORÉM SERIA NECESSÁRIO A CRIAÇÃO DE UM CAMPO DO TIPO SENHA -> É UM DADO SENSIVÉL
+    // ENTÃO NÃO É RECOMENDÁVEL POR ESSA QUESTÃO
+
     fun autentica(senha: Int): Boolean
 
 }
