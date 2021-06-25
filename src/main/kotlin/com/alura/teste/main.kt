a3352c52727333838b9bd4ae252491084a07ab05
Daniel <daniel.fecaf@outlook.com>
2021-06-25 08:38:01 -0300

    Exceptions -> Utilizando try catch

--- src/main/kotlin/com/alura/teste/main.kt
+++ src/main/kotlin/com/alura/teste/main.kt
@@ -1,10 +1,16 @@
+import com.alura.modelo.Endereco
+import java.lang.ArithmeticException
+import java.lang.ClassCastException
+
 fun main() {
     println("início main")
+
+
     funcao1()
     println("fim main")
 }
 
-fun funcao1(){
+fun funcao1() {
     println("início funcao1")
     funcao2()
     println("fim funcao1")
@@ -12,8 +18,17 @@ fun funcao1(){
 
 fun funcao2() {
     println("início funcao2")
-    for (i in 1..5){
-        println(i)
+
+    try {
+        for (i in 1..5) {
+            println(i)
+            val endereco = Any()
+            endereco as Endereco
+        }
+    } catch (e: ClassCastException) {
+        println(e.stackTrace)
+
+
     }
     println("fim funcao2")
 }
