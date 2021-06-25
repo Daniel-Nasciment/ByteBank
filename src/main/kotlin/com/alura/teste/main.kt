5465d2c59ae98af72f1160e37f7befd997775332
Daniel <daniel.fecaf@outlook.com>
2021-06-25 07:43:47 -0300

    Testando debugger

--- src/main/kotlin/com/alura/teste/main.kt
+++ src/main/kotlin/com/alura/teste/main.kt
@@ -1,13 +1,21 @@
-import com.alura.modelo.Endereco
-
 fun main() {
-
-    val endereco = Endereco()
-
-    println(endereco.toString())
-
-
+    println("início main")
+    funcao1()
+    println("fim main")
 }
 
+fun funcao1(){
+    println("início funcao1")
+    funcao2()
+    println("fim funcao1")
+}
+
+fun funcao2() {
+    println("início funcao2")
+    for (i in 1..5){
+        println(i)
+    }
+    println("fim funcao2")
+}
 
 
