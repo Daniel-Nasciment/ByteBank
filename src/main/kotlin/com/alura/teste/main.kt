702f1e6431d96657f516bb349164a0bf6aa66fcd
Daniel <daniel.fecaf@outlook.com>
2021-06-27 20:22:03 -0300

    Array de objetos - BigDecimal

--- src/main/kotlin/com/alura/teste/main.kt
+++ src/main/kotlin/com/alura/teste/main.kt
@@ -1,37 +1,44 @@
+package com.alura.teste
+
+import java.math.BigDecimal
+import java.math.RoundingMode
+
 fun main() {
 
-    val idades: IntArray = intArrayOf(10, 15, 18, 20)
+    // COMO É UM ARRAY NÃO PRIMITIVO DA LINGUAGEM DEVEMOS SETAR OS VALORES DE INICIALIZAÇÃO
+    val salarios = bigDecimalArrayOf("1500.0", "2050.45")
 
-    // MAIOR NÚMERO OU NULL
-    val maiorIdade = idades.maxOrNull()
+    // CONTENT TOSTRING RETORNA A STRING DO ARRAY
+    println(salarios.contentToString())
 
-    // MENOR NÚMERO OU NULL
-    val menorIdade = idades.minOrNull()
+    val aumento = "1.1".toBigDecimal()
+    val salariosComAumento: Array<BigDecimal> =
+        salarios.map { salario ->
+            if (salario < "5000".toBigDecimal()){
+                salario + "500".toBigDecimal()
+            }else{
+                    // SETSCALE (QUANRTIDADE DE CASAS, MÉTODO DE ARREDONDAMENTO)
+                (salario * aumento).setScale(2, RoundingMode.UP)
+            }
+        }.toTypedArray()
 
-    println(maiorIdade)
-    println(menorIdade)
+    println(salariosComAumento.contentToString())
 
-    // CALCULA A MÉDIA
-    val media = idades.average()
-
-    println(media)
-
-    // .ALL RETORNA TRUE QUANDO TODOS OS VALORES SATISFAZEREM A CONDIÇÃO
-    val todos = idades.all { it > 18 }
-
-    // .ANY RETORNA TRUE QUANDO APENAS UMA SATISFIZER A CONDIÇÃO
-    val algum = idades.any { it > 18 }
-
-    // CRIA UMA LISTA ONDE SATISFAZ A CONDIÇÃO
-    val listaMaiores = idades.filter { it >= 18 }
-    println(listaMaiores)
-
-    // RETORNA O PRIMEIRO ITEM QUE SATISFAZ A CONDIÇÃO, OU SEJA, SE TIVER DOIS VALORES IGUAIS
-    // O MÉTODO FIND RETORNARÁ O PRIMEIRO
-    val busca = idades.find { it == 18 }
-
+    //ASSIM COMO NOS TIPOS PRIMITIVOS POSSIMOS COMO POR EXEMPLO UM IntArrayOf
+    // NO CASO DO BIG DECIMAL SERIA NECESSÁRIO A CRIAÇÃO DE UM MÉTODO QUE FAÇA O MESMO
 
 }
+
+// VARARGS RETORNA UM ARRAY COM A QUANTIDADE DE VALORES CONTIDOS
+fun bigDecimalArrayOf(vararg valores: String): Array<BigDecimal> {
+    // COMO 'VALORES É UM ARRAY' -> SIZE SERIA O TAMANHO DO MESMO
+    return Array<BigDecimal>(valores.size) {
+        // I -> INDICE DO ARRAY -> VARIAÇÃO DE ACORDO COM O TAMANHO DO ARRAY (DE 0 A 2 POR EX)
+            i ->
+        valores[i].toBigDecimal()
+    }
+}
+
 
 
 
