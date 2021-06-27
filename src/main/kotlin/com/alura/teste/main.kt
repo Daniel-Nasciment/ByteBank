c90087f1a902bcbada62d0f1883f589ac92abf60
Daniel <daniel.fecaf@outlook.com>
2021-06-27 17:54:20 -0300

    Operações agregadoras

--- src/main/kotlin/com/alura/teste/main.kt
+++ src/main/kotlin/com/alura/teste/main.kt
@@ -1,47 +1,35 @@
 fun main() {
 
-    val series: IntRange = 1.rangeTo(10)
+    val idades: IntArray = intArrayOf(10, 15, 18, 20)
 
-    for (s in series){
-        print("${s} ")
-    }
+    // MAIOR NÚMERO OU NULL
+    val maiorIdade = idades.maxOrNull()
 
+    // MENOR NÚMERO OU NULL
+    val menorIdade = idades.minOrNull()
 
-    println()
+    println(maiorIdade)
+    println(menorIdade)
 
-    // A DECLARAÇÃO A CIMA É A MESMA QUE:
+    // CALCULA A MÉDIA
+    val media = idades.average()
 
-    // SETEP -> VAI SER CONTADO DE 2 EM 2
-    // 0.until(10) -> Não inclusiva, ou seja, não vai até o 10
-    
-    val numeros = 1..10 step 2
-    // DENTRO DO FOR -> O IN ATRIBUI A VARIAVEL NUMERO CADA ITEM DE NUMEROS
-    for (numero in numeros){
-        print("${numero} ")
-    }
+    println(media)
 
+    // .ALL RETORNA TRUE QUANDO TODOS OS VALORES SATISFAZEREM A CONDIÇÃO
+    val todos = idades.all { it > 18 }
 
-    // DECRECENTE
+    // .ANY RETORNA TRUE QUANDO APENAS UMA SATISFIZER A CONDIÇÃO
+    val algum = idades.any { it > 18 }
 
-    println()
+    // CRIA UMA LISTA ONDE SATISFAZ A CONDIÇÃO
+    val listaMaiores = idades.filter { it >= 18 }
+    println(listaMaiores)
 
-    val decrescentes = 10 downTo 0 step 2
+    // RETORNA O PRIMEIRO ITEM QUE SATISFAZ A CONDIÇÃO, OU SEJA, SE TIVER DOIS VALORES IGUAIS
+    // O MÉTODO FIND RETORNARÁ O PRIMEIRO
+    val busca = idades.find { it == 18 }
 
-    decrescentes.forEach { print("${it} ") }
-
-
-    println()
-
-
-    val intervalo = 1000.0..5000.0
-    val salario = 3500.0
-
-    // DENTRO DO IF -> O IN VERIFICA SE TAL VALOR ESTÁ DENTRO DO INTERVALO
-    if(salario in intervalo){
-        println("True")
-    }else{
-        println("False")
-    }
 
 }
 
