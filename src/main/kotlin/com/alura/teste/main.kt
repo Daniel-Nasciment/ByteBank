a21a4a3a64cbfb75b81464898461e8936867aa2a
Daniel <daniel.fecaf@outlook.com>
2021-06-28 01:29:09 -0300

    Trabalhando com associate

--- src/main/kotlin/com/alura/teste/main.kt
+++ src/main/kotlin/com/alura/teste/main.kt
@@ -3,11 +3,28 @@ package com.alura.teste
 
 fun main() {
 
-    val lista01 = setOf("A", "B", "C")
-    val lista02 = setOf("A", "D", "E")
-    val total = lista01 + lista02
+    val pedidos = listOf<Pedido>(
+        Pedido(1, 50.0),
+        Pedido(2, 60.0),
+        Pedido(3, 70.0),
+        Pedido(4, 80.0),
+        Pedido(5, 90.0)
+    )
 
-    println(total)
+    println(pedidos)
+    val pedidosMapeados: Map<Int, Pedido> = pedidos.associateBy { pedido -> pedido.numero }
+    val pedidosFrete: Map<Pedido, Boolean> = pedidos.associateWith { pedido -> pedido.valor > 50.0 }
+
+    println(pedidosMapeados)
+    println(pedidosFrete)
+}
+
+
+data class Pedido(
+    val numero: Int,
+    val valor: Double
+) {
+
 }
 
 
