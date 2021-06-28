626300f59ff1d1dff8a398826b2f5864594dd99e
Daniel <daniel.fecaf@outlook.com>
2021-06-27 21:39:54 -0300

    Trabalhando com listas

--- src/main/kotlin/com/alura/teste/main.kt
+++ src/main/kotlin/com/alura/teste/main.kt
@@ -1,75 +1,52 @@
 package com.alura.teste
 
-import java.math.BigDecimal
-import java.math.RoundingMode
-
 fun main() {
 
-    // COMO É UM ARRAY NÃO PRIMITIVO DA LINGUAGEM DEVEMOS SETAR OS VALORES DE INICIALIZAÇÃO
-    val salarios = bigDecimalArrayOf("1500.0", "2050.45", "5000.0", "2300.0")
-
-    // CONTENT TOSTRING RETORNA A STRING DO ARRAY
-    println(salarios.contentToString())
-
-    val aumento = "1.1".toBigDecimal()
-    val salariosComAumento: Array<BigDecimal> =
-        salarios.map { salario ->
-            calculaAumento(salario, aumento)
-        }.toTypedArray()
-
-    println(salariosComAumento.contentToString())
+    val livro1 = Livro(
+        titulo = "Livro 1",
+        autor = "Autor 1",
+        anoPublic = 2000
+    )
 
 
-    // DO MENOR PARA O MAIOR
-
-    /*val salariosOrdenados = salariosComAumento.sorted()
-    val tresUltimosSalarios: Array<BigDecimal> = salariosOrdenados.takeLast(3).toTypedArray()
-    val media = tresUltimosSalarios.media()*/
-
-    val media = salariosComAumento
-        .sorted()
-        .takeLast(3)
-        .toTypedArray()
-        .media()
+    val livro2 = Livro(
+        titulo = "Livro 2",
+        autor = "Autor 2",
+        anoPublic = 2005,
+        editora = "Editora teste"
+    )
 
 
-    println(media)
+    val livro3 = Livro(
+        titulo = "Livro 3",
+        autor = "Autor 3",
+        anoPublic = 2010
+    )
 
+
+    val livro4 = Livro(
+        titulo = "Livro 4",
+        autor = "Autor 4",
+        anoPublic = 2020,
+        editora = "Editora teste"
+    )
+
+
+    val listaLivros = mutableListOf<Livro>(livro1, livro2, livro3)
+
+
+    listaLivros.add(livro4)
+
+    println(listaLivros)
 
 }
 
-private fun calculaAumento(salario: BigDecimal, aumento: BigDecimal) =
-    if (salario < "5000".toBigDecimal()) {
-        salario + "500".toBigDecimal()
-    } else {
-        // SETSCALE (QUANRTIDADE DE CASAS, MÉTODO DE ARREDONDAMENTO)
-        (salario * aumento).setScale(2, RoundingMode.UP)
-    }
-
-// VARARGS RETORNA UM ARRAY COM A QUANTIDADE DE VALORES CONTIDOS
-fun bigDecimalArrayOf(vararg valores: String): Array<BigDecimal> {
-    // COMO 'VALORES É UM ARRAY' -> SIZE SERIA O TAMANHO DO MESMO
-    return Array<BigDecimal>(valores.size) {
-        // I -> INDICE DO ARRAY -> VARIAÇÃO DE ACORDO COM O TAMANHO DO ARRAY (DE 0 A 2 POR EX)
-            i ->
-        valores[i].toBigDecimal()
-    }
-}
-
-fun Array<BigDecimal>.somatoria(): BigDecimal {
-
-    return this.reduce { acumulador, valor ->
-        acumulador + valor
-    }
-}
-
-fun Array<BigDecimal>.media(): BigDecimal {
-    return if (this.isEmpty()) {
-        BigDecimal.ZERO
-    } else {
-        this.somatoria() / this.size.toBigDecimal()
-    }
-}
+data class Livro(
+    val titulo: String,
+    val autor: String,
+    val anoPublic: Long,
+    val editora: String? = null
+)
 
 
 
