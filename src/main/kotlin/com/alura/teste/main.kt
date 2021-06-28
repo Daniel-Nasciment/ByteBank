3585a6d0f72e0d7156c7c6db1906aa38a25f4d8c
Daniel <daniel.fecaf@outlook.com>
2021-06-28 15:28:58 -0300

    Criando variaveis do tipo função

--- src/main/kotlin/com/alura/teste/main.kt
+++ src/main/kotlin/com/alura/teste/main.kt
@@ -3,33 +3,21 @@ package com.alura.teste
 
 fun main() {
 
-    val pedidos = listOf<Pedido>(
-        Pedido(1, 50.0),
-        Pedido(2, 60.0),
-        Pedido(3, 70.0),
-        Pedido(4, 80.0),
-        Pedido(5, 90.0)
-    )
+    val minhaFuncao: () -> Unit = ::teste
+    println(minhaFuncao())
 
-    println(pedidos)
-    val pedidosMapeados: Map<Int, Pedido> = pedidos.associateBy { pedido -> pedido.numero }
-    val pedidosFrete: Map<Pedido, Boolean> = pedidos.associateWith { pedido -> pedido.valor > 50.0 }
-
-    val pedidosFreteGratisAgrupado: Map<Boolean, List<Pedido>> =
-        pedidos.groupBy { pedido: Pedido ->
-            pedido.valor > 50.0
-        }
-
-    println(pedidosMapeados)
-    println(pedidosFrete)
-    println(pedidosFreteGratisAgrupado[true])
+    val minhaFuncaoClasse = Teste()
+    println(minhaFuncaoClasse())
 }
 
+fun teste(){
 
-data class Pedido(
-    val numero: Int,
-    val valor: Double
-) {
+}
+
+class Teste : () -> Unit {
+    override fun invoke() {
+        println("Executa invoke teste")
+    }
 
 }
 
