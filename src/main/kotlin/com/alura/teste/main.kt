acc8a1ecde99b1d0a3ae4dbf95e221f127ce829d
Daniel <daniel.fecaf@outlook.com>
2021-06-28 16:46:13 -0300

    Explorando implementações com let -> Chamadas encadeadas

--- src/main/kotlin/com/alura/teste/main.kt
+++ src/main/kotlin/com/alura/teste/main.kt
@@ -4,19 +4,20 @@ import com.alura.modelo.Endereco
 
 
 fun main() {
-    val testeFuncao: () -> Unit
-    Endereco().let {
-        it
-    }
-    "".let(::testeRecebeString)
-    1.let {
-        it
-    }
 
+//    val endereco = Endereco(logradouro = "Rua teste", numero = 1000)
+//    val enderecoEmMaiusculo = "${endereco.logradouro}, ${endereco.numero}".toUpperCase()
+//    println(enderecoEmMaiusculo)
+
+
+    // ESSE CÓDIGO SERIA O MESMO QUE O DE CIMA
+    // CHAMADA ENCADEADA PARA EVITAR A CRIAÇÃO DE VARIÁVEIS É BEM COMUM
+    Endereco(logradouro = "Rua teste", numero = 1000)
+        .let {endereco ->
+            "${endereco.logradouro}, ${endereco.numero}".toUpperCase()
+        }
+        .let (::println)
 }
 
-fun testeRecebeString(valor: String){
-
-}
 
 
