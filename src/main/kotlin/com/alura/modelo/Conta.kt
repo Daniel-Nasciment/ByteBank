74beeb6a76936eeb300b39fe8db7e3d242dde8b4
Daniel <daniel.fecaf@outlook.com>
2021-06-28 21:22:09 -0300

    Testando RUN

--- src/main/kotlin/com/alura/modelo/Conta.kt
+++ src/main/kotlin/com/alura/modelo/Conta.kt
@@ -20,7 +20,6 @@ abstract class Conta(
     }
 
     init {
-        println("Criando conta")
         Contador.total++
     }
 
--- src/main/kotlin/com/alura/teste/main.kt
+++ src/main/kotlin/com/alura/teste/main.kt
@@ -1,20 +1,39 @@
 package com.alura.teste
 
+import com.alura.modelo.Cliente
+import com.alura.modelo.ContaPoupanca
 import com.alura.modelo.Endereco
 
 
 fun main() {
 
-    // TESTANDO O WITH
+    // TESTANDO O RUN
 
-    val endComp = with(Endereco()) {
-        logradouro = "Rua teste"
-        numero = 1234
-        completo()
-    }.let {
-        println(it)
+    testaRun()
+}
+
+fun testaRun() {
+    val taxaAnual = 0.05
+    val taxaMensal = taxaAnual / 12
+    println("taxa mensal $taxaMensal")
+
+    val contaPoupanca = ContaPoupanca(Cliente(nome = "Alex", cpf = "111.111.111-11", senha = 1234), 1000)
+
+    contaPoupanca.run {
+        deposito(1000.0)
+        saldo * taxaMensal
+    }.let { rendimentoMensal ->
+        println("rendimento mensal $rendimentoMensal")
     }
 
+    val rendimentoAnual = run {
+        var saldo = contaPoupanca.saldo
+        repeat(12) {
+            saldo += saldo * taxaMensal
+        }
+        saldo
+    }
+    println("simulação rendimento anual $rendimentoAnual")
 }
 
 
